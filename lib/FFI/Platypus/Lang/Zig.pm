2a969bddfd4fba9e18f3c046fbea4f057fc2b16a
Graham Ollis <plicease@cpan.org>
2022-10-27 16:43:08 -0600

    Initial structure

---
+++ lib/FFI/Platypus/Lang/Zig.pm
@@ -0,0 +1,12 @@
+package FFI::Platypus::Lang::Zig;
+
+use strict;
+use warnings;
+use 5.008004;
+
+# ABSTRACT: Documentation and tools for using Platypus with the Zig programming language
+# VERSION
+
+1;
+
+
